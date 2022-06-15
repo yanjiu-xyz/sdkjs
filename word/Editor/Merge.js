96a7b6f2c9a3fd8f29f9aff7fbe0e2095059b6a1
Vladimir Privezenov <privezenoff@yandex.ru>
2022-06-15 21:10:27 +0300

    Add imports and consts

--- word/Editor/Merge.js
+++ word/Editor/Merge.js
@@ -1,3 +1,11 @@
 (function (undefined) {
+    var CDocumentComparison = AscCommonWord.CDocumentComparison;
+    var CNode               = AscCommonWord.CNode;
+    var g_oTableId          = AscCommon.g_oTableId;
+
+    var EXCLUDED_PUNCTUATION = {};
+    EXCLUDED_PUNCTUATION[46] = true; // TODO: organize import
+    //EXCLUDED_PUNCTUATION[95] = true;
+    EXCLUDED_PUNCTUATION[160] = true;
 
 })()
