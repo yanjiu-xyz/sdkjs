db60a472b16ca50a12b7847fe26b8067ce47826d
Aleksandr <aleksandr.nagaev@onlyoffice.com>
2022-09-18 18:00:34 +0300

    [SE] show-as pivot updated

--- cell/model/Workbook.js
+++ cell/model/Workbook.js
@@ -8253,7 +8253,7 @@
 								fieldItem = field.getItem(colItem.x[colItemsXIndex].getV());
 								curDataRow = curDataRow.subtotal[fieldItem.x];
 								if (curNextDataRow && curNextDataRow.subtotal) {
-									if (fieldIndex == curBaseField && field.getItem(rowItem.x[rowItemsXIndex].getV() + 1)) {
+									if (fieldIndex == curBaseField && field.getItem(colItem.x[colItemsXIndex].getV() + 1)) {
 										curNextDataRow = curNextDataRow.subtotal[field.getItem(colItem.x[colItemsXIndex].getV() + 1).x];
 									} else {
 										curNextDataRow = curNextDataRow.subtotal[field.getItem(colItem.x[colItemsXIndex].getV()).x];
