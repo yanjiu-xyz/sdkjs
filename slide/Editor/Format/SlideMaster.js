8aee69d8f85053953ae9ba7c77f5c0b6867492ce
Sergey Luzyanin <Sergey.Luzyanin@onlyoffice.com>
2023-03-20 21:31:02 +0530

    remove console.log

--- slide/Editor/Format/SlideMaster.js
+++ slide/Editor/Format/SlideMaster.js
@@ -182,7 +182,6 @@ MasterSlide.prototype.draw = function (graphics, slide) {
 		let nTime = (new Date()).getTime();
         if (this.cSld.spTree[i].isPlaceholder && !this.cSld.spTree[i].isPlaceholder())
             this.cSld.spTree[i].draw(graphics);
-		console.log("ID "  + this.cSld.spTree[i].Get_Id() + " Time: " + ((new Date()).getTime() - nTime))
     }
 };
 MasterSlide.prototype.getMatchingLayout = function (type, matchingName, cSldName, themeFlag) {
