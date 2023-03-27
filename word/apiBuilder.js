87a0124bdc3c1002037108651b0e74945a670250
KirillovIlya <ilya.kirillov@onlyoffice.com>
2023-03-28 00:13:46 +0500

    [bu][de] Add a description of the returned object to GetReviewReport

--- word/apiBuilder.js
+++ word/apiBuilder.js
@@ -5678,12 +5678,38 @@
 
 		return oResult;
 	};
-
+	
+	/**
+	 * Review record type
+	 * @typedef {("TextAdd" | "TextRem" | "ParaAdd" | "ParaRem" | "TextPr" | "ParaPr" | "Unknown")} ReviewReportRecordType
+	 */
+	
+	/**
+	 * Record of one review change
+	 * @typedef {Object} ReviewReportRecord
+	 * @property {ReviewReportRecordType} Type - change type
+	 * @property {string} [Value=undefined] - value is set for types "TextAdd" and "TextRem" only
+	 * @property {number} Date - date-time when this change was made
+	 */
+	
+	/**
+	 * Report on all review changes
+	 * This is a dictionary where the keys are usernames
+	 * @typedef {Object.<string, Array.<ReviewReportRecord>>} ReviewReport
+	 * @example
+	 * 	{
+	 * 	  "John Smith" : [{Type: 'TextRem', Value: 'Hello, Mark!', Date: 1679941734161},
+	 * 	                {Type: 'TextAdd', Value: 'Dear Mr. Pottato.', Date: 1679941736189}],
+	 * 	  "Mark Pottato" : [{Type: 'ParaRem', Date: 1679941755942},
+	 * 	                  {Type: 'TextPr', Date: 1679941757832}]
+	 * 	}
+	 */
+	
 	/**
 	 * Returns a report about every change which was made to the document in the review mode.
 	 * @memberof ApiDocument
 	 * @typeofeditors ["CDE"]
-	 * @returns {object}
+	 * @returns ReviewReport
 	 */
 	ApiDocument.prototype.GetReviewReport = function()
 	{
