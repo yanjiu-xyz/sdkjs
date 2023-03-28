3f4055fae21c9caf614ca8cc5b212f5905a11427
Sergey Linnik <sergey.linnik@onlyoffice.com>
2023-03-28 11:28:26 +0500

    Fix documentation

--- word/apiBuilder.js
+++ word/apiBuilder.js
@@ -5694,7 +5694,7 @@
 	 * Returns a report about every change which was made to the document in the review mode.
 	 * @memberof ApiDocument
 	 * @typeofeditors ["CDE"]
-	 * @returns ReviewReport
+	 * @returns {ReviewReport}
 	 */
 	ApiDocument.prototype.GetReviewReport = function()
 	{
