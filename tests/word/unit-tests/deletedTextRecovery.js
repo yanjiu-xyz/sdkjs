d41a6e5b270f01dc96894d2d8b816f1617ff1ca5
EvgeniyIgol <evgeny.igol@onlyoffice.com>
2023-07-05 18:15:38 +0300

    [de] Add tests for restore deleted text - improve work of restore deleted text

--- tests/word/unit-tests/deletedTextRecovery.js
+++ tests/word/unit-tests/deletedTextRecovery.js
@@ -31,12 +31,233 @@
  */
 
 $(function () {
-	
-	QUnit.module("Unit-tests for recover deleted text");
-	
-	
-	QUnit.test("Test:", function (assert)
+	let logicDocument = AscTest.CreateLogicDocument();
+	function AddParagraph(pos)
 	{
+		let p = AscTest.CreateParagraph();
+		logicDocument.AddToContent(pos, p);
+		return p;
+	}
+	function CreateRun(text)
+	{
+		let r = AscTest.CreateRun();
+		r.AddText(text);
+		return r;
+	}
+	function DelLast(nCount)
+	{
+		for (let i = 0; i < nCount; i++)
+			AscTest.PressKey(AscTest.Key.backspace);
+	}
+	function Recover()
+	{
+		let oDeletedText = new AscCommon.DeletedTextRecovery(true);
+		let nLengthOfPoints = AscCommon.History.Points.length - 1;
+		let arr = [];
 
+		for (let i = 0; i <= nLengthOfPoints; i++)
+			AscCommon.History.GetChangesFromPoint(i, arr);
+
+		oDeletedText.oColloborativeHistory.Changes = arr;
+		oDeletedText.Changes = arr;
+		oDeletedText.InitRevision();
+		oDeletedText.ShowDel();
+	}
+
+	function CheckRuns(assert, paragraph, arr)
+	{
+		for (let i = 0; i < paragraph.Content.length - 1; i++)
+		{
+			if (!arr[i])
+				return;
+
+			let name = arr[i][1] === reviewtype_Common ? "reviewtype_Common" : "reviewtype_Remove";
+			let str ="\"" + arr[i][0] + "\"" + " is " + name
+			let oCurrentRun = paragraph.Content[i];
+				assert.strictEqual(oCurrentRun.ReviewType, arr[i][1], str);
+		}
+	}
+
+	QUnit.testStart (function (){
+		AscCommon.History.Clear();
+		AscTest.ClearDocument();
+		//AscCommon.History.CreateNewPointToCollectChanges(AscDFH.historydescription_Collaborative_Undo);
+	})
+	QUnit.testDone(function () {
+		//AscCommon.History.Remove_LastPoint();
+	})
+
+
+	QUnit.module("Unit-tests for recover deleted text");
+
+	QUnit.test("Del one letter", function (assert)
+	{
+		let strStartText = "abc";
+		let p = AddParagraph(0);
+		let run = CreateRun(strStartText);
+		p.AddToContentToEnd(run);
+		assert.ok(true, "Create run with 'abc' text.");
+
+		DelLast(1);
+		assert.ok(true, "Delete one last letter");
+		let strDeletedText = AscTest.GetParagraphText(p);
+		assert.strictEqual(strDeletedText, "ab", "Text in run is 'ab'");
+
+		Recover();
+		assert.ok(true, "Recover deleted text");
+		let recoverRun = p.Content[1];
+		assert.strictEqual(recoverRun.ReviewType, reviewtype_Remove, "New ParaRun ReviewType is delete");
+		assert.strictEqual(run.ReviewType, reviewtype_Common, "Old ParaRun ReviewType is common");
+
+		let strResultText = AscTest.GetParagraphText(p);
+		assert.strictEqual(strResultText, "abc", "Text in run is 'abc'");
+
+		CheckRuns(assert, p, [
+			["ab", reviewtype_Common],
+			[" c", reviewtype_Remove],
+		]);
+	});
+
+	QUnit.test("Del many letter", function (assert)
+	{
+		let strStartText = "Hello World";
+		let p = AddParagraph(0);
+		let run = CreateRun(strStartText);
+		p.AddToContentToEnd(run);
+		assert.ok(true, "Create run with '" + strStartText+"' text.");
+
+		DelLast(6);
+		assert.ok(true, "Delete ' World'");
+		let strDeletedText = AscTest.GetParagraphText(p);
+		assert.strictEqual(strDeletedText, "Hello", "Text in run is 'Hello'");
+
+		Recover();
+		assert.ok(true, "Recover deleted text");
+		let strResultText = AscTest.GetParagraphText(p);
+		assert.strictEqual(strResultText, "Hello World", "Text in run is 'Hello World'");
+
+		CheckRuns(assert, p, [
+			["Hello", reviewtype_Common],
+			[" World", reviewtype_Remove],
+		]);
+	});
+
+	QUnit.test("Del many letter in middle", function (assert)
+	{
+		let strStartText = "Hello World";
+		let p = AddParagraph(0);
+		let run = CreateRun(strStartText);
+		p.AddToContentToEnd(run);
+		assert.ok(true, "Create run with '" + strStartText+"' text.");
+
+		AscTest.MoveCursorLeft(false, false, 6);
+		DelLast(4);
+		assert.ok(true, "Delete 'llo'");
+		let strDeletedText = AscTest.GetParagraphText(p);
+		assert.strictEqual(strDeletedText, "H World", "Text in run is 'H World'");
+
+		Recover();
+		assert.ok(true, "Recover deleted text");
+		let strResultText = AscTest.GetParagraphText(p);
+		assert.strictEqual(strResultText, "Hello World", "Text in run is 'Hello World'");
+
+		CheckRuns(assert, p, [
+			["He", reviewtype_Common],
+			["llo", reviewtype_Remove],
+			[" World", reviewtype_Common],
+		]);
+	});
+
+	QUnit.test("Del many letter in middle 2", function (assert)
+	{
+		let strStartText = "Hello World";
+		let p = AddParagraph(0);
+		let run = CreateRun(strStartText);
+		p.AddToContentToEnd(run);
+		assert.ok(true, "Create run with '" + strStartText+"' text.");
+
+		AscTest.MoveCursorLeft(false, false, 8);
+		DelLast(2);
+		assert.ok(true, "Delete 'el'");
+		let strDeletedText = AscTest.GetParagraphText(p);
+		assert.strictEqual(strDeletedText, "Hlo World", "Text in run is 'Hlo World'");
+
+		AscTest.MoveCursorRight(false, false, 6);
+		DelLast(2);
+		assert.ok(true, "Delete 'or'");
+		strDeletedText = AscTest.GetParagraphText(p);
+		assert.strictEqual(strDeletedText, "Hlo Wld", "Text in run is 'Hlo Wld'");
+
+		Recover();
+		assert.ok(true, "Recover deleted text");
+		let strResultText = AscTest.GetParagraphText(p);
+		assert.strictEqual(strResultText, "Hello World", "Text in run is 'Hello World'");
+
+		CheckRuns(assert, p, [
+			["H", reviewtype_Common],
+			["el", reviewtype_Remove],
+			["lo W", reviewtype_Common],
+			["or", reviewtype_Remove],
+			["ld", reviewtype_Common],
+		]);
+	});
+
+	QUnit.test("Del many letter in middle 3", function (assert)
+	{
+		let strStartText = "Hello World";
+		let p = AddParagraph(0);
+		let run = CreateRun(strStartText);
+		p.AddToContentToEnd(run);
+		assert.ok(true, "Create run with '" + strStartText+"' text.");
+
+		AscTest.MoveCursorLeft(false, false, 2);
+		DelLast(2);
+		assert.ok(true, "Delete 'or'");
+		let strDeletedText = AscTest.GetParagraphText(p);
+		assert.strictEqual(strDeletedText, "Hello Wld", "Text in run is 'Hello Wld'");
+
+		AscTest.MoveCursorLeft(false, false, 4);
+		DelLast(2);
+		assert.ok(true, "Delete 'el'");
+		strDeletedText = AscTest.GetParagraphText(p);
+		assert.strictEqual(strDeletedText, "Hlo Wld", "Text in run is 'Hlo Wld'");
+
+		debugger
+		Recover();
+		assert.ok(true, "Recover deleted text");
+		let strResultText = AscTest.GetParagraphText(p);
+		assert.strictEqual(strResultText, "Hello World", "Text in run is 'Hello World'");
+
+		CheckRuns(assert, p, [
+			["H", reviewtype_Common],
+			["el", reviewtype_Remove],
+			["lo W", reviewtype_Common],
+			["or", reviewtype_Remove],
+			["ld", reviewtype_Common],
+		]);
+	});
+
+	QUnit.test("Del paragraph", function (assert)
+	{
+		let strOne = "One";
+		let p = AddParagraph(0);
+		let run = CreateRun(strOne);
+		p.AddToContentToEnd(run);
+
+		DelLast(5);
+		assert.ok(true, "Delete 'One'");
+
+		let strDeletedText = AscTest.GetParagraphText(p);
+		assert.strictEqual(strDeletedText, "", "Text in document is ''");
+
+		Recover();
+		assert.ok(true, "Recover deleted text");
+		let strResultText = AscTest.GetParagraphText(p);
+		assert.strictEqual(strResultText, "One", "Text in run is 'One'");
+
+		CheckRuns(assert, p, [
+			["One", reviewtype_Remove],
+		]);
+		console.log(p);
 	});
 });
