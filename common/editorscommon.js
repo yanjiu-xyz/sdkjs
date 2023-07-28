57579fe01ccbd3d679d2b47ac1fd000347da3028
Sergey Konovalov <Sergey.Konovalov@onlyoffice.com>
2023-07-28 16:43:28 +0300

    [all] Add svg as supported image ext. For bug 37564

--- common/editorscommon.js
+++ common/editorscommon.js
@@ -1673,7 +1673,7 @@
 	//todo get from server config
 	var c_oAscImageUploadProp = {//Не все браузеры позволяют получить информацию о файле до загрузки(например ie9), меняя параметры здесь надо поменять аналогичные параметры в web.common
 		MaxFileSize:      25000000, //25 mb
-		SupportedFormats: ["jpg", "jpeg", "jpe", "png", "gif", "bmp"]
+		SupportedFormats: ["jpg", "jpeg", "jpe", "png", "gif", "bmp", "svg"]
 	};
 
 	var c_oAscDocumentUploadProp = {
