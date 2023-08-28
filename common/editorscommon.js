592a5780e9b3c4f99364969659f86a9de700190a
Sergey Konovalov <Sergey.Konovalov@onlyoffice.com>
2023-08-28 19:56:58 +0300

    [all] Console.log performance metrics with action "debug"

--- common/editorscommon.js
+++ common/editorscommon.js
@@ -13204,6 +13204,9 @@
 		if (!api) {
 			return;
 		}
+		if (api.documentOpenOptions && api.documentOpenOptions["debug"]) {
+			console.log("clientLog: "+ msg);
+		}
 		api.CoAuthoringApi.sendClientLog(level, msg);
 	}
 
