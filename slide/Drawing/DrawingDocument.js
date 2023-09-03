5471b8c1bcf448a7341920abd07828f5acb3082d
Sergey Luzyanin <Sergey.Luzyanin@onlyoffice.com>
2023-09-03 21:12:01 +0300

    [bug] fix bug 63792

--- slide/Drawing/DrawingDocument.js
+++ slide/Drawing/DrawingDocument.js
@@ -5295,7 +5295,7 @@ function CThumbnailsManager()
 	{
 		this.InitCheckOnResize();
 		var word_control = this.m_oWordControl;
-
+		let oPresentation = word_control.m_oLogicDocument;
 		var dKoefToPix = AscCommon.AscBrowser.retinaPixelRatio * g_dKoef_mm_to_pix;
 
 		var __w = word_control.m_oThumbnailsContainer.AbsolutePosition.R - word_control.m_oThumbnailsContainer.AbsolutePosition.L;
@@ -5315,7 +5315,7 @@ function CThumbnailsManager()
 		if (this.DigitWidths.length > 5)
 			_tmpDig = this.DigitWidths[5];
 
-		this.const_offset_x = (_tmpDig * dKoefToPix * (("") + (this.SlidesCount + 1)).length) >> 0;
+		this.const_offset_x = (_tmpDig * dKoefToPix * (("") + (this.SlidesCount + oPresentation.getFirstSlideNumber())).length) >> 0;
 		if (this.const_offset_x < 25)
 			this.const_offset_x = 25;
 
