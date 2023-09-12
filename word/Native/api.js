e6088763bafec359dde6f9c35c40410267e5daca
Pavel Chernyshev <39688331+chernyshevpavel@users.noreply.github.com>
2023-09-12 17:28:47 +0300

    Feature/mobile columns settings (#3737)
    [native][de] Add columns settings

--- word/Native/api.js
+++ word/Native/api.js
@@ -4467,6 +4467,67 @@ Asc["asc_docs_api"].prototype["asc_nativeSetContentControlDatePickerDate"] = fun
     _api.asc_SetContentControlDatePickerPr(oPr, sId, true);
 }
 
+/**
+ * one - 0
+ * two - 1
+ * three - 2
+ * left - 3
+ * right - 4 
+ * @param {*} sId 
+ * @returns 
+ */
+Asc["asc_docs_api"].prototype["asc_nativeSetColumnsSettings"] = function(sId) {
+    var props = new Asc.CDocumentColumnsProps(),
+                        cols = sId,
+                        def_space = 12.5;
+                    props.put_EqualWidth(cols<3);
+    if (cols<3) {
+        props.put_Num(cols+1);
+        props.put_Space(def_space);
+    } else {
+        var total = _api.asc_GetColumnsProps().get_TotalWidth(),
+            left = (total - def_space*2)/3,
+            right = total - def_space - left;
+        props.put_ColByValue(0, (cols == 3) ? left : right, def_space);
+        props.put_ColByValue(1, (cols == 3) ? right : left, 0);
+        props.colbyva
+    }
+    _api.asc_SetColumnsProps(props);
+    
+}
+
+/**
+ * one - 0
+ * two - 1
+ * three - 2
+ * left - 3
+ * right - 4 
+ * @returns (-1, 0, 1, 2, 3, 4)
+ */
+Asc["asc_docs_api"].prototype["asc_getNativeSetColumnsSettings"] = function() {
+    var props = _api.asc_GetColumnsProps();
+    var equal = props.get_EqualWidth(),
+        num = (equal) ? props.get_Num() : props.get_ColsCount(),
+        def_space = 12.5,
+        index = -1;
+    if (equal && num<4 && (num==1 ||  Math.abs(props.get_Space() - def_space)<0.1))
+        index = (num-1);
+    else if (!equal && num==2) {
+        var left = props.get_Col(0).get_W(),
+            space = props.get_Col(0).get_Space(),
+            right = props.get_Col(1).get_W(),
+            total = props.get_TotalWidth();
+        if (Math.abs(space - def_space)<0.1) {
+            var width = (total - space*2)/3;
+            if ( left<right && Math.abs(left - width)<0.1 )
+                index = 3;
+            else if (left>right && Math.abs(right - width)<0.1)
+                index = 4;
+        }
+    }
+    return index;
+}
+
 Asc["asc_docs_api"].prototype["asc_nativeAddText"] = function(text, wrapWithSpaces) {
     var settings = new AscCommon.CAddTextSettings();
 
