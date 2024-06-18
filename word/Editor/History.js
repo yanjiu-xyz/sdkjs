fb9cfaa9e286d6b742732553daa2b5e3616ed64c
KirillovIlya <ilya.kirillov@onlyoffice.com>
2024-06-18 18:37:44 +0300

    Fix bug #63879
    Treat the paste operation as an AsYouType operation

--- word/Editor/History.js
+++ word/Editor/History.js
@@ -1512,15 +1512,47 @@ CHistory.prototype.private_PostProcessingRecalcData = function()
 		if ((AscDFH.historydescription_Document_AddLetter === oPoint.Description
 			|| AscDFH.historydescription_Document_AddLetterUnion === oPoint.Description
 			|| AscDFH.historydescription_Document_SpaceButton === oPoint.Description
-			|| AscDFH.historydescription_Presentation_ParagraphAdd === oPoint.Description)
+			|| AscDFH.historydescription_Presentation_ParagraphAdd === oPoint.Description
+			|| AscDFH.historydescription_Document_PasteHotKey === oPoint.Description)
 			&& nItemsCount > 0)
 		{
-			var oChange = oPoint.Items[nItemsCount - 1].Data;
-			if (!oChange || !oChange.IsContentChange())
-				return false;
-
-			var nChangeItemsCount = oChange.GetItemsCount();
-			return (nChangeItemsCount > 0 && AscDFH.historyitem_ParaRun_AddItem === oChange.GetType() && oChange.GetItem(nChangeItemsCount - 1) === oLastElement);
+			if (AscDFH.historydescription_Document_PasteHotKey === oPoint.Description)
+			{
+				// Последний добавленный элемент во время вставки был до AscDFH.historyitem_ParaRun_OnStartSplit
+				let splitCounter = 0;
+				for (let changeIndex = oPoint.Items.length - 1; changeIndex >= 0; --changeIndex)
+				{
+					let change = oPoint.Items[changeIndex].Data;
+					if (!change)
+						continue;
+					
+					if (AscDFH.historyitem_ParaRun_OnEndSplit === change.GetType())
+					{
+						++splitCounter;
+						continue;
+					}
+					else if (AscDFH.historyitem_ParaRun_OnStartSplit === change.GetType())
+					{
+						--splitCounter;
+						continue;
+					}
+					
+					if (splitCounter > 0 || !change.IsContentChange() || AscDFH.historyitem_ParaRun_AddItem !== change.GetType())
+						continue;
+					
+					let nChangeItemsCount = change.GetItemsCount();
+					return (nChangeItemsCount > 0 && change.GetItem(nChangeItemsCount - 1) === oLastElement);
+				}
+			}
+			else
+			{
+				var oChange = oPoint.Items[nItemsCount - 1].Data;
+				if (!oChange || !oChange.IsContentChange())
+					return false;
+				
+				let nChangeItemsCount = oChange.GetItemsCount();
+				return (nChangeItemsCount > 0 && AscDFH.historyitem_ParaRun_AddItem === oChange.GetType() && oChange.GetItem(nChangeItemsCount - 1) === oLastElement);
+			}
 		}
 
 		return false;
