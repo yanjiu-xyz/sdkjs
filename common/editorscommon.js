d7b777aa64036ec9ecf1dba35a6e6ebca2b3de54
Sergey Konovalov <Sergey.Konovalov@onlyoffice.com>
2024-06-26 20:39:41 +0300

    [all] Add .tiff to supported image formats; for bug 45401

--- common/editorscommon.js
+++ common/editorscommon.js
@@ -1668,7 +1668,7 @@
 	//todo get from server config
 	var c_oAscImageUploadProp = {//Не все браузеры позволяют получить информацию о файле до загрузки(например ie9), меняя параметры здесь надо поменять аналогичные параметры в web.common
 		MaxFileSize:      25000000, //25 mb
-		SupportedFormats: ["jpg", "jpeg", "jpe", "png", "gif", "bmp", "svg"]
+		SupportedFormats: ["jpg", "jpeg", "jpe", "png", "gif", "bmp", "svg", "tiff", "tif"]
 	};
 
 	var c_oAscDocumentUploadProp = {
