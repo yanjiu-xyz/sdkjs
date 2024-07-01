709ac86fe68f2c3698564cb0526589c638d20378
EvgeniyIgol <evgeny.igol@onlyoffice.com>
2024-07-01 14:52:54 +0300

    Fix bug #68941

--- word/Math/NamesOfLiterals.js
+++ word/Math/NamesOfLiterals.js
@@ -3652,10 +3652,11 @@
 	}
 	function RemoveCountFormMathContent (oContent, nCount, isSkipFirst)
 	{
+		let oCurrentContent;
 		for (let i = oContent.Content.length - 1; i >= 0; i--)
 		{
 			let isSkippedFirst = false;
-			let oCurrentContent = oContent.Content[i];
+			oCurrentContent = oContent.Content[i];
 			for (let j = oCurrentContent.Content.length - 1; j >= 0; j--)
 			{
 				if (isSkipFirst === true)
@@ -3671,6 +3672,7 @@
 					return oCurrentContent;
 			}
 		}
+		return oCurrentContent;
 	}
 
 	function CorrectSpecialWordOnCursor(oCMathContent, IsLaTeX)
