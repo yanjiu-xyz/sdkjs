182ff8103df19598169557cea515de79aebd5dd9
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2024-09-27 12:02:17 +0300

    Fix input in telegram web view

--- common/text_input2.js
+++ common/text_input2.js
@@ -942,7 +942,7 @@
 		{
 			_style = ("left:-" + (this.HtmlAreaWidth >> 1) + "px;top:" + (-this.HtmlAreaOffset) + "px;");
 			_style += "color:transparent;caret-color:transparent;background:transparent;";
-			_style += AscCommon.AscBrowser.isAppleDevices ? "font-size:0px;" : "font-size:8px;";
+			_style += (AscCommon.AscBrowser.isAppleDevices && !AscCommon.AscBrowser.isTelegramWebView) ? "font-size:0px;" : "font-size:8px;";
 		}
 		else
 		{
