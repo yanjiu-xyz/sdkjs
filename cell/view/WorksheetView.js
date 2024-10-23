4a769ccd03aeb0684d51e28ba1884744c62e7c32
GoshaZotov <Igor.Zotov@onlyoffice.com>
2024-10-23 17:35:03 +0300

    [se] By bug 71172: clean highlight headers after move from headers

--- cell/view/WorksheetView.js
+++ cell/view/WorksheetView.js
@@ -11566,7 +11566,7 @@
 
 		let isMobileVersion = this.workbook && this.workbook.Api && this.workbook.Api.isMobileVersion;
 		var epsChangeSize = AscCommon.AscBrowser.convertToRetinaValue(3 * AscCommon.global_mouseEvent.KoefPixToMM, true);
-		if (x <= this.cellsLeft && y >= this.cellsTop && x >= this.headersLeft) {
+		if (x <= this.cellsLeft && y >= this.cellsTop && x >= this.headersLeft + this.getRightToLeftOffset()) {
 			r = this._findRowUnderCursor(y, true);
 			if (r === null) {
 				return oResDefault;
