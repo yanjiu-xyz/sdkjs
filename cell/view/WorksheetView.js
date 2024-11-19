80138bf72b344d635a00b5bc64bd9829c3714fa2
GoshaZotov <Igor.Zotov@onlyoffice.com>
2024-11-19 11:35:09 +0300

    [se] Fix bug 71597

--- cell/view/WorksheetView.js
+++ cell/view/WorksheetView.js
@@ -1556,6 +1556,18 @@
             this.updateResize = false;
 
             this.objectRender.resizeCanvas();
+			if (this.getRightToLeft()) {
+				let drawings = this.objectRender.controller.getDrawingObjects();
+				for (var i = 0; i < drawings.length; ++i) {
+					if (!drawings[i].group) {
+						AscFormat.CheckSpPrXfrm3(drawings[i], true);
+					} else {
+						AscFormat.CheckSpPrXfrm(drawings[i], true);
+					}
+				}
+				this.objectRender.controller.recalculate(true);
+			}
+
 			if (editor) {
 				editor.move();
 			}
